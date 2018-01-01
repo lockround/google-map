9648bc2cbb8c4bbcacbaf1410ba17b71fc44f777
lockround <websbiz0000@gmail.com>
2018-01-01 19:44:24 +0530

    Intial Commit

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+// including plugins
+var gulp = require('gulp'),
+uglify = require("gulp-uglify"),
+minifyHtml = require("gulp-minify-html"),
+minifyCss = require("gulp-minify-css");
+
+// task
+gulp.task('minify-js', function () {
+    gulp.src('map.js') // path to your files
+    .pipe(uglify())
+    .pipe(gulp.dest('dest'));
+});
+
+//CSS Task
+// task
+gulp.task('minify-css', function () {
+    gulp.src('style.css') // path to your file
+    .pipe(minifyCss())
+    .pipe(gulp.dest('dest'));
+});
+
+// HTML task
+gulp.task('minify-html', function () {
+    gulp.src('map.html') // path to your files
+    .pipe(minifyHtml())
+    .pipe(gulp.dest('dest'));
+});
+
+gulp.task('default', [ 'minify-html', 'minify-js', 'minify-css' ]);
---
+++ map.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var apiKey = 'AIzaSyBs_M0n7mAmFkecJKEnCqcyKnfnfvzzgu4';
+var api = 'https://maps.googleapis.com/maps/api/geocode/json';
+
+var box = document.getElementById('zipcode');
+box.addEventListener('keypress', findLocation);
+
+function findLocation(){
+
+	var zipcode = document.getElementById('zipcode').value;
+	var url = api+'?address='+zipcode+'&key='+apiKey;
+
+	//Fetch API
+	fetch(url).then(function(response){
+		if(!response.ok){
+			throw Error(response.status);
+		}
+		return response.json();
+	}).then(function(response){
+		handleResponse(response);
+	}).catch(function(error){
+		console.log("XHR Failed");
+	});
+}
+	function handleResponse(data){
+		outputLocation(data);
+	}
+
+function outputLocation(data){
+	var location = document.getElementById('location');
+	var geo = data.results[0].geometry.location;
+	location.innerHTML = "<p>"+data.results[0].formatted_address+"</p>"+"<p>Lattitude: "+geo.lat+"<br/>"+"Longitude: "+geo.lng+"</p>";
+}
